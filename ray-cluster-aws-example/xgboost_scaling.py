31b19c2b9d13dc894ba9ab6b0f45dbf06dba9676
andymc629 <andrewpmcmahon629@gmail.com>
2023-10-23 20:44:06 +0100

    xgboost script before I butcher it

--- ray-cluster-aws-example/xgboost_scaling.py
+++ ray-cluster-aws-example/xgboost_scaling.py
@@ -2,27 +2,25 @@ import ray
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from ray.air.config import ScalingConfig
+#from ray.air.config import ScalingConfig
+from ray.train import ScalingConfig
 from ray.train.xgboost import XGBoostTrainer
 
 
 # Create a preprocessor to scale some columns.
 from ray.data.preprocessors import StandardScaler
 
-def train_xgboost_model(num_workers, num_cpus, train_dataset, 
-                        valid_dataset, preprocessor):
+def train_xgboost_model(num_workers, num_cpus, train_dataset, valid_dataset):
     trainer = XGBoostTrainer(
         scaling_config=ScalingConfig(
             # Number of workers to use for data parallelism.
             num_workers=num_workers,
             resources_per_worker={'CPU': num_cpus},
             # Whether to use GPU acceleration.
-            use_gpu=False,
-            # Make sure to leave some CPUs free for Ray Data operations.
-            _max_cpu_fraction_per_node=0.9,
+            use_gpu=False
     ),
     label_column="target",
-    num_boost_round=20,
+    num_boost_round=500,#100,#40,#20,
     params={
         # XGBoost specific params
         "objective": "binary:logistic",
@@ -30,19 +28,17 @@ def train_xgboost_model(num_workers, num_cpus, train_dataset,
         "eval_metric": ["logloss", "error"],
     },
     datasets={"train": train_dataset, "valid": valid_dataset},
-    preprocessor=preprocessor,
     )
     result = trainer.fit()
     return result
 
-def run_experiment(train_dataset, valid_dataset, preprocessor):
+def run_experiment(train_dataset, valid_dataset):
     trial_results = []
 
     num_workers=1
     for num_cpus in [1,2,3,4,5,6]:
         result = train_xgboost_model(num_workers=num_workers, num_cpus=num_cpus, 
-                                    train_dataset=train_dataset, valid_dataset=valid_dataset, 
-                                    preprocessor=preprocessor)
+                                    train_dataset=train_dataset, valid_dataset=valid_dataset)
         result_dict = result.metrics
         result_dict['num_workers'] = num_workers
         result_dict['num_cpus'] = num_cpus
@@ -51,8 +47,7 @@ def run_experiment(train_dataset, valid_dataset, preprocessor):
     num_workers=2
     for num_cpus in [1,2,3]:
         result = train_xgboost_model(num_workers=num_workers, num_cpus=num_cpus,
-                                    train_dataset=train_dataset, valid_dataset=valid_dataset, 
-                                    preprocessor=preprocessor)
+                                    train_dataset=train_dataset, valid_dataset=valid_dataset)
         result_dict = result.metrics
         result_dict['num_workers'] = num_workers
         result_dict['num_cpus'] = num_cpus
@@ -60,11 +55,25 @@ def run_experiment(train_dataset, valid_dataset, preprocessor):
 
     return pd.DataFrame(trial_results) 
 
+def prep_dataset(dataset, partitions):
+    # Split data into train and validation.
+    train_dataset, valid_dataset = dataset.repartition(partitions).train_test_split(test_size=0.3)
+
+    # Create a test dataset by dropping the target column.
+    test_dataset = valid_dataset.drop_columns(cols=["target"])
+    
+    preprocessor = StandardScaler(columns=["mean radius", "mean texture"])
+    train_dataset_transformed = preprocessor.fit_transform(train_dataset)
+    valid_dataset_transformed = preprocessor.transform(valid_dataset)
+    
+    return train_dataset_transformed, valid_dataset_transformed
+    
+
 def main():
     ray.init()
     # Load data from standard s3 example bucket
     # Repartition the data into 6 partitions to enable parallelization
-    dataset = ray.data.read_csv("s3://anonymous@air-example-data/breast_cancer.csv").repartition(6)
+    dataset = ray.data.read_csv("s3://anonymous@air-example-data/breast_cancer.csv").repartition(3)
 
     # Split data into train and validation.
     train_dataset, valid_dataset = dataset.train_test_split(test_size=0.3)
@@ -73,9 +82,12 @@ def main():
     test_dataset = valid_dataset.drop_columns(cols=["target"])
     
     preprocessor = StandardScaler(columns=["mean radius", "mean texture"])
+    train_dataset_transformed = preprocessor.fit_transform(train_dataset)
+    valid_dataset_transformed = preprocessor.transform(valid_dataset)
+      
+    trial_results = run_experiment(train_dataset=train_dataset_transformed, valid_dataset=valid_dataset_transformed)
     
-    trial_results = run_experiment(train_dataset=train_dataset, valid_dataset=valid_dataset, 
-                                   preprocessor=preprocessor)
+    print(trial_results)
     
     plt.rcParams.update({'font.size': 22})
 
